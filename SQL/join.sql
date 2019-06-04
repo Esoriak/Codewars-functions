ae9ba3656abe6b8d2f1b283caa1857d3d716e44c
Esoriak <somahaut.dev@gmail.com>
2019-06-04 10:26:09 +0200

    add join and group by codewars

--- SQL/join.sql
+++ SQL/join.sql
@@ -4,3 +4,8 @@ select p.*, count(t) as toy_count from people p
 join toys t
 on t.people_id = p.id
 group by p.id
+
+SQL Basics: Simple JOIN 
+
+SELECT products.*, companies.name AS company_name
+FROM products JOIN companies ON company_id = companies.id*
